99391e3f2814db2a8c8cd6f02301b8a4d08118e5
Ahmad Ibrahim <malikahmadibrahim332@gmail.com>
2026-02-27 23:15:03 +0500

    added buttons

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let display = document.getElementById("display");
 let buttons = document.querySelectorAll("button");
 let equal = document.getElementById("cal");
+let clear = document.getElementById("clear");
+let delBtn = document.getElementById("del");
 
 buttons.forEach((btn) => {
   btn.addEventListener("click", () => {
@@ -17,4 +19,10 @@ equal.addEventListener("click", () => {
   display.value = result;
 });
 
+clear.addEventListener("click", () => {
+  display.value = "";
+});
 
+delBtn.addEventListener("click", () => {
+  display.value = display.value.slice(0, -1);
+});
